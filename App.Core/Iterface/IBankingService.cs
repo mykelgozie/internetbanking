9f3a711d0da392ab4c7b360eee31e5aacc97ef7b
mykelgozie <mykelgozie@gmail.com>
2021-04-26 23:57:17 +0100

    created controller

---
+++ App.Core/Iterface/IBankingService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Core.Iterface
+{
+    public interface IBankingService
+    {
+
+        public Task<string> GetApiVersion();
+
+        public Task<string> CalculateMD5();
+
+        public Task<Boolean> IsPasswordStrong();
+    }
+}
